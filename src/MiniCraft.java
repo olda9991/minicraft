90251293f7ccf0e79c2b27a8c85282a2d6d26973
olda <oldagordie@gmail.com>
2026-07-18 11:25:56 +0200

    v6.4.3: made worlds biger

--- src/MiniCraft.java
+++ src/MiniCraft.java
@@ -82,6 +82,7 @@ import java.util.ArrayList;
 import javax.sound.sampled.*;
 
 public class MiniCraft extends JPanel implements ActionListener, KeyListener, MouseListener, MouseMotionListener, MouseWheelListener {
+//------------------ts is how big world is-------------
     private static final int TILE = 32, W = 256, H = 64, VW = 25, VH = 18;
     private static final String VERSION = "6.4.2";
     private static final String GITHUB_API = "https://api.github.com/repos/olda9991/minicraft/commits/main";
